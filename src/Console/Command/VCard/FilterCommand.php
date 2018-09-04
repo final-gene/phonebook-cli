1e600709042f2026d28e18bd24bb73a9dc953922
Frank Giesecke <frank.giesecke@final-gene.de>
2018-09-04 22:58:54 +0200

    Filter command

--- src/Console/Command/VCard/FilterCommand.php
+++ src/Console/Command/VCard/FilterCommand.php
@@ -9,16 +9,16 @@ namespace FinalGene\PhoneBook\Console\Command\VCard;
 
 use FinalGene\PhoneBook\Console\Command\FileTrait;
 use FinalGene\PhoneBook\Console\Command\VCardTrait;
-use FinalGene\PhoneBook\Exception\EmptyFileException;
 use FinalGene\PhoneBook\Exception\ReadFileException;
 use Sabre\VObject\Component\VCard;
-use Sabre\VObject\Document;
-use Sabre\VObject\Reader;
+use Sabre\VObject\ParseException;
+use Sabre\VObject\Splitter\VCard as VCardList;
 use SplFileInfo;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -41,6 +41,11 @@ class FilterCommand extends Command
      * Command exit code if the input could not be used
      */
     public const EXIT_INPUT_ERROR = 1;
+
+    /**
+     * Command exit code if an invalid vCard is given
+     */
+    public const EXIT_VCARD_ERROR = 2;
 
     /**
      * Command title
@@ -68,6 +73,26 @@ class FilterCommand extends Command
     public const INPUT_FILE_ARGUMENT_DESCRIPTION = 'Input file path';
 
     /**
+     * Name of note filter option
+     */
+    public const NOTE_FILTER_OPTION_NAME = 'filter-note';
+
+    /**
+     * Description of note filter option
+     */
+    public const NOTE_FILTER_OPTION_DESCRIPTION = 'Filter cards with matching note content';
+
+    /**
+     * Name of has telephone filter option
+     */
+    public const HAS_TELEPHONE_FILTER_OPTION_NAME = 'filter-has-telephone';
+
+    /**
+     * Description of has telephone filter option
+     */
+    public const HAS_TELEPHONE_FILTER_OPTION_DESCRIPTION = 'Filter cards with telephone information';
+
+    /**
      * @var SymfonyStyle
      */
     private $io;
@@ -88,6 +113,20 @@ class FilterCommand extends Command
             InputArgument::OPTIONAL,
             self::INPUT_FILE_ARGUMENT_DESCRIPTION,
             'php://stdin'
+        );
+
+        $this->addOption(
+            self::NOTE_FILTER_OPTION_NAME,
+            null,
+            InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
+            self::NOTE_FILTER_OPTION_DESCRIPTION
+        );
+
+        $this->addOption(
+            self::HAS_TELEPHONE_FILTER_OPTION_NAME,
+            null,
+            InputOption::VALUE_NONE,
+            self::HAS_TELEPHONE_FILTER_OPTION_DESCRIPTION
         );
     }
 
@@ -117,18 +156,35 @@ class FilterCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): ?int
     {
+        $filter = [
+            self::NOTE_FILTER_OPTION_NAME => (array)$input->getOption(self::NOTE_FILTER_OPTION_NAME),
+            self::HAS_TELEPHONE_FILTER_OPTION_NAME => $input->getOption(self::HAS_TELEPHONE_FILTER_OPTION_NAME),
+        ];
+
         try {
-            $vCard = $this->getVCardByFile(
+            $vCardList = $this->getVCardListByFile(
                 new SplFileInfo(
                     $input->getArgument(self::INPUT_FILE_ARGUMENT_NAME)
                 )
             );
-        } catch (EmptyFileException|ReadFileException $e) {
+        } catch (ReadFileException $e) {
             $this->io->error($e->getMessage());
+
             return self::EXIT_INPUT_ERROR;
         }
 
-        $output->write($vCard->serialize());
+        /** @var VCard $vCard */
+        try {
+            while (null !== ($vCard = $vCardList->getNext())) {
+                if ($this->filter($vCard, $filter)) {
+                    $output->write($vCard->serialize());
+                }
+            }
+        } catch (ParseException $e) {
+            $this->io->error($e->getMessage());
+
+            return self::EXIT_VCARD_ERROR;
+        }
 
         return self::EXIT_OK;
     }
@@ -138,14 +194,97 @@ class FilterCommand extends Command
      *
      * @param SplFileInfo $file
      *
-     * @return VCard
-     * @throws EmptyFileException
+     * @return VCardList
      * @throws ReadFileException
      */
-    protected function getVCardByFile(SplFileInfo $file): VCard
+    protected function getVCardListByFile(SplFileInfo $file): VCardList
     {
-        /** @var VCard $vCard */
-        $vCard = Reader::read($this->readFile($file));
-        return $vCard->convert(Document::VCARD40);
+        return new VCardList($this->getResourceForFile($file));
+    }
+
+    /**
+     * Filter.
+     *
+     * @param VCard $vCard
+     * @param array $filter
+     *
+     * @return bool
+     */
+    protected function filter(VCard $vCard, array $filter): bool
+    {
+        $success = true;
+        foreach ($filter as $filterOptionName => $filterValues) {
+            $result = $this->{self::getFilterFunctionNameByFilterOptionName($filterOptionName)}($vCard, $filterValues);
+            $success = $success && $result;
+        }
+
+        return $success;
+    }
+
+    /**
+     * Filter note.
+     *
+     * @param VCard $vCard
+     * @param array $filterValueList
+     *
+     * @return bool
+     */
+    protected function filterByNote(VCard $vCard, array $filterValueList): bool
+    {
+        $filterValueList = array_filter($filterValueList);
+        if (empty($filterValueList)) {
+            return true;
+        }
+
+        if (isset($vCard->note)) {
+            foreach ($filterValueList as $filterValue) {
+                if (false !== strpos((string)$vCard->note, $filterValue)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Filter by has telephone.
+     *
+     * @param VCard     $vCard
+     * @param bool|null $mustHaveTelephone
+     *
+     * @return bool
+     */
+    protected function filterByHasTelephone(VCard $vCard, ?bool $mustHaveTelephone): bool
+    {
+        if (null === $mustHaveTelephone) {
+            return true;
+        }
+
+        return !empty($vCard->select('TEL'));
+    }
+
+    /**
+     * Get filter name.
+     *
+     * @param string $filterOptionName
+     *
+     * @return string
+     */
+    public static function getFilterFunctionNameByFilterOptionName(string $filterOptionName): string
+    {
+        return 'filterBy' . str_replace(
+                '-',
+                '',
+                ucwords(
+                    str_replace(
+                        'filter-',
+                        '',
+                        $filterOptionName
+                    ),
+                    '-'
+                )
+            );
+
     }
 }
--- tests/Console/Command/VCard/FilterCommandTest.php
+++ tests/Console/Command/VCard/FilterCommandTest.php
@@ -7,12 +7,15 @@
 
 namespace FinalGene\PhoneBook\Console\Command\VCard;
 
+use FinalGene\PhoneBook\Exception\ReadFileException;
 use FinalGene\PhoneBook\Utils\TestHelperTrait;
 use org\bovigo\vfs\vfsStream;
 use org\bovigo\vfs\vfsStreamWrapper;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
 use Sabre\VObject\Component\VCard;
-use Sabre\VObject\Document;
+use Sabre\VObject\ParseException;
+use Sabre\VObject\Splitter\VCard as VCardList;
 use SplFileInfo;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\NullOutput;
@@ -29,6 +32,16 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class FilterCommandTest extends TestCase
 {
     use TestHelperTrait;
+
+    /**
+     * @var array
+     */
+    private const FILTER_LIST = [
+        FilterCommand::NOTE_FILTER_OPTION_NAME => [
+            'some note',
+        ],
+        FilterCommand::HAS_TELEPHONE_FILTER_OPTION_NAME => true,
+    ];
 
     /**
      * Set up before class
@@ -107,21 +120,53 @@ VCF
             ->shouldBeCalled()
             ->willReturn('');
 
+        foreach (array_keys(self::FILTER_LIST) as $filterOptionName) {
+            $input->getOption($filterOptionName)
+                ->shouldBeCalled()
+                ->willReturn(null);
+        }
+
         $output = $this->prophesize(OutputInterface::class);
+        $output->write('vCardContent')
+            ->shouldBeCalled();
 
         $vCard = $this->prophesize(VCard::class);
+        $vCard->serialize()
+            ->shouldBeCalledTimes(1)
+            ->willReturn('vCardContent');
+
+        $vCardList = $this->prophesize(VCardList::class);
+        $vCardList->getNext()
+            ->shouldBeCalled()
+            ->willReturn(
+                $vCard->reveal(),
+                $vCard->reveal(),
+                null
+            );
 
         $command = $this->createPartialMock(
             FilterCommand::class,
             [
-                'getVCardByFile',
+                'getVCardListByFile',
+                'filter',
             ]
         );
 
         $command->expects(static::once())
-            ->method('getVCardByFile')
+            ->method('getVCardListByFile')
             ->with(static::isInstanceOf(SplFileInfo::class))
-            ->willReturn($vCard->reveal());
+            ->willReturn($vCardList->reveal());
+
+        $command->expects(static::exactly(2))
+            ->method('filter')
+            ->with(
+                static::isInstanceOf(VCard::class),
+                static::isType('array')
+            )
+            ->willReturn(
+                true,
+                false
+            );
 
         static::assertEquals(
             FilterCommand::EXIT_OK,
@@ -137,24 +182,353 @@ VCF
     }
 
     /**
-     * Test get vcard by file.
+     * Test command execution will fail on read error.
      *
      * @return void
      */
-    public function testGetVCardByFile(): void
+    public function testCommandExecutionWillFailOnInputError(): void
+    {
+        $input = $this->prophesize(InputInterface::class);
+        $input->getArgument(FilterCommand::INPUT_FILE_ARGUMENT_NAME)
+            ->shouldBeCalled()
+            ->willReturn('');
+
+        $input->getOption(Argument::any())
+            ->shouldBeCalled()
+            ->willReturn(null);
+
+        $io = $this->prophesize(SymfonyStyle::class);
+        $io->error(Argument::type('string'))
+            ->shouldBeCalled();
+
+        $command = $this->createPartialMock(
+            FilterCommand::class,
+            [
+                'getVCardListByFile',
+            ]
+        );
+
+        $command->expects(static::once())
+            ->method('getVCardListByFile')
+            ->with(static::isInstanceOf(SplFileInfo::class))
+            ->willThrowException(new ReadFileException(''));
+
+        static::setPropertyValue($command, 'io', $io->reveal());
+
+        static::assertEquals(
+            FilterCommand::EXIT_INPUT_ERROR,
+            static::invokeMethod(
+                $command,
+                'execute',
+                [
+                    $input->reveal(),
+                    $this->createMock(OutputInterface::class)
+                ]
+            )
+        );
+    }
+
+    /**
+     * Test command execution will fail on parse error.
+     *
+     * @return void
+     */
+    public function testCommandExecutionWillFailOnVCardError(): void
+    {
+        $input = $this->prophesize(InputInterface::class);
+        $input->getArgument(FilterCommand::INPUT_FILE_ARGUMENT_NAME)
+            ->shouldBeCalled()
+            ->willReturn('');
+
+        $input->getOption(Argument::any())
+            ->shouldBeCalled()
+            ->willReturn(null);
+
+        $vCardList = $this->prophesize(VCardList::class);
+        $vCardList->getNext()
+            ->shouldBeCalled()
+            ->willThrow(ParseException::class);
+
+        $io = $this->prophesize(SymfonyStyle::class);
+        $io->error(Argument::type('string'))
+            ->shouldBeCalled();
+
+        $command = $this->createPartialMock(
+            FilterCommand::class,
+            [
+                'getVCardListByFile',
+            ]
+        );
+
+        $command->expects(static::once())
+            ->method('getVCardListByFile')
+            ->with(static::isInstanceOf(SplFileInfo::class))
+            ->willReturn($vCardList->reveal());
+
+        static::setPropertyValue($command, 'io', $io->reveal());
+
+        static::assertEquals(
+            FilterCommand::EXIT_VCARD_ERROR,
+            static::invokeMethod(
+                $command,
+                'execute',
+                [
+                    $input->reveal(),
+                    $this->createMock(OutputInterface::class)
+                ]
+            )
+        );
+    }
+
+    /**
+     * Test get vcard list by file.
+     *
+     * @return void
+     */
+    public function testGetVCardListByFile(): void
     {
         $command = $this->createPartialMock(FilterCommand::class, []);
 
         /** @var VCard $result */
         $result = static::invokeMethod(
             $command,
-            'getVCardByFile',
+            'getVCardListByFile',
             [
                 new SplFileInfo(vfsStream::url('root/test.vcf')),
             ]
         );
 
-        static::assertInstanceOf(VCard::class, $result);
-        static::assertEquals(Document::VCARD40, $result->getDocumentType());
+        static::assertInstanceOf(VCardList::class, $result);
+    }
+
+    /**
+     * Test filter will succeed if all filters match.
+     *
+     * @param bool $match
+     * @param bool $expected
+     *
+     * @return void
+     * @dataProvider dataForFilterTest
+     */
+    public function testFilter(bool $match, bool $expected): void
+    {
+        $filterByMethodList = [];
+        foreach (array_keys(self::FILTER_LIST) as $filterOptionName) {
+            $filterByMethodList[$filterOptionName] = FilterCommand::getFilterFunctionNameByFilterOptionName($filterOptionName);
+        }
+
+        $command = $this->createPartialMock(
+            FilterCommand::class,
+            $filterByMethodList
+        );
+
+        foreach ($filterByMethodList as $filterOptionName => $functionName) {
+            $command->expects(static::once())
+                ->method($functionName)
+                ->with(
+                    static::isInstanceOf(VCard::class),
+                    self::FILTER_LIST[$filterOptionName]
+                )
+                ->willReturn($match);
+        }
+
+        static::assertEquals(
+            $expected,
+            static::invokeMethod(
+                $command,
+                'filter',
+                [
+                    $this->createMock(VCard::class),
+                    self::FILTER_LIST,
+                ]
+            )
+        );
+    }
+
+    /**
+     * Data for filter test.
+     *
+     * @return array
+     */
+    public function dataForFilterTest(): array
+    {
+        return [
+            'filter will succeed if all filters match' => [
+                'match' => true,
+                'expected' => true,
+            ],
+            'filter will fail if not all filters match' => [
+                'match' => false,
+                'expected' => false,
+            ],
+        ];
+    }
+
+    /**
+     * Test filter by note.
+     *
+     * @param VCard $vCard
+     * @param array $filterValueList
+     * @param bool  $expected
+     *
+     * @return void
+     * @dataProvider dataForFilterByNoteTest
+     */
+    public function testFilterByNote(VCard $vCard, array $filterValueList, bool $expected): void
+    {
+        $command = $this->createPartialMock(FilterCommand::class, []);
+
+        static::assertEquals(
+            $expected,
+            static::invokeMethod(
+                $command,
+                'filterByNote',
+                [
+                    $vCard,
+                    $filterValueList,
+                ]
+            )
+        );
+    }
+
+    /**
+     * Data for filter by note test.
+     *
+     * @return array
+     */
+    public function dataForFilterByNoteTest(): array
+    {
+        $vCardWithNote = new VCard(
+            [
+                'NOTE' => 'some note',
+            ]
+        );
+
+        return [
+            'succeed if no values are given' => [
+                'vCard' => $this->createMock(VCard::class),
+                'filterValueList' => (array)null,
+                'expected' => true,
+            ],
+            'succeed if list of empty values is given' => [
+                'vCard' => $this->createMock(VCard::class),
+                'filterValueList' => [
+                    '',
+                    null,
+                ],
+                'expected' => true,
+            ],
+            'fail if values are given and vCard has no note' => [
+                'vCard' => $this->createMock(VCard::class),
+                'filterValueList' => [
+                    'foo',
+                ],
+                'expected' => false,
+            ],
+            'fail if values are given but will not match on vCard note' => [
+                'vCard' => $vCardWithNote,
+                'filterValueList' => [
+                    'foo',
+                ],
+                'expected' => false,
+            ],
+            'succeed if one value will match on vCard note' => [
+                'vCard' => $vCardWithNote,
+                'filterValueList' => [
+                    'foo',
+                    'some',
+                ],
+                'expected' => true,
+            ],
+        ];
+    }
+
+    /**
+     * Test filter by note.
+     *
+     * @param VCard     $vCard
+     * @param bool|null $filterValue
+     * @param bool      $expected
+     *
+     * @return void
+     * @dataProvider dataForFilterByHasTelephoneTest
+     */
+    public function testFilterByHasTelephone(VCard $vCard, ?bool $filterValue, bool $expected): void
+    {
+        $command = $this->createPartialMock(FilterCommand::class, []);
+
+        static::assertEquals(
+            $expected,
+            static::invokeMethod(
+                $command,
+                'filterByHasTelephone',
+                [
+                    $vCard,
+                    $filterValue,
+                ]
+            )
+        );
+    }
+
+    /**
+     * Data for filter by note test.
+     *
+     * @return array
+     */
+    public function dataForFilterByHasTelephoneTest(): array
+    {
+        return [
+            'succeed if no value is given' => [
+                'vCard' => $this->createMock(VCard::class),
+                'filterValue' => null,
+                'expected' => true,
+            ],
+            'succeed if vCard has telephone information' => [
+                'vCard' => new VCard(['TEL' => '0123456789']),
+                'filterValue' => true,
+                'expected' => true,
+            ],
+            'fail if vCard has no telephone information' => [
+                'vCard' => $this->createMock(VCard::class),
+                'filterValue' => true,
+                'expected' => false,
+            ],
+        ];
+    }
+
+    /**
+     * Test get filter name.
+     *
+     * @param string $optionName
+     * @param string $expected
+     *
+     * @return void
+     * @dataProvider dataForGetFilterFunctionNameByFilterOptionNameTest
+     */
+    public function testGetFilterFunctionNameByFilterOptionName(string $optionName, string $expected): void
+    {
+        static::assertEquals(
+            $expected,
+            FilterCommand::getFilterFunctionNameByFilterOptionName($optionName)
+        );
+    }
+
+    /**
+     * Data for get filter function name by filter option name test.
+     *
+     * @return array
+     */
+    public function dataForGetFilterFunctionNameByFilterOptionNameTest(): array
+    {
+        return [
+            'note filter' => [
+                'optionName' => FilterCommand::NOTE_FILTER_OPTION_NAME,
+                'expected' => 'filterByNote',
+            ],
+            'telephone filter' => [
+                'optionName' => FilterCommand::HAS_TELEPHONE_FILTER_OPTION_NAME,
+                'expected' => 'filterByHasTelephone',
+            ],
+        ];
     }
 }
